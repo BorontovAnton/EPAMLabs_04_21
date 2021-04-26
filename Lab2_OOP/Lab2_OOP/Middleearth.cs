7629805063a7f31337bcb0b1c4af007cca0b4807
BorontovAnton <borontov.anton@gmail.com>
2021-04-26 23:12:37 +0300

    end HW2

---
+++ Lab2_OOP/Lab2_OOP/Middleearth.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_OOP
+{
+    abstract class Middleearth
+    {
+        public string Race { get; set; }
+        public string Region { get; set; }
+        public string Weapon { get; set; }
+
+        public abstract string battlecry();         //абстрактный метод выводит боевой клич для рассы
+        public Middleearth(string race, string region, string weapon)
+
+        {
+            Race = race;
+            Region = region;
+            Weapon = weapon;
+        }
+
+        public void Affiliation()                   //Метод выводит параметры рассы
+        {
+            Console.WriteLine(Race);
+            Console.WriteLine(Region);
+            Console.WriteLine(Weapon);
+        }
+
+    }
+}
---
+++ Lab2_OOP/Lab2_OOP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab2_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gnome gnome = new Gnome ("Гном", "Одинокая гора", "Топор", "Гимли", "Глоин");
+            Orc orc = new Orc ("Орк", "Мордор", "Ятаган", 30, 4, 2);
+
+            gnome.Affiliation();
+            Console.WriteLine($"{gnome.Name} сын {gnome.Ancestor}а");
+            Console.WriteLine($"Боевой клич = {gnome.battlecry()}");
+            Console.WriteLine("-------------------");
+            orc.Affiliation();
+            Console.WriteLine($"Убийств на счету = {orc.kills}");
+            Console.WriteLine($"Боевой клич = {orc.battlecry()}");
+
+        }
+    }
+}
