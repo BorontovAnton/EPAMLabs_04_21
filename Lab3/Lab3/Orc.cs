eba1e6a6dc6f5cc0ae169397736e15fa69807503
BorontovAnton <borontov.anton@gmail.com>
2021-05-01 22:36:44 +0300

    End lab3

---
+++ Lab3/Lab3/Orc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_OOP
+{
+    class Orc : Middleearth
+    {
+        public int totalkills;
+        public int Humans { get; set; }
+        public int Gnoms   { get; set; }
+        public int Elfs { get; set; }
+        public Orc () 
+        {
+            this.totalkills = Humans + Gnoms + Elfs;
+        }
+        
+        public override string battlecry()
+        {
+            return "Waaagh";
+        }
+    }
+}
+
